908796e86f0f9145abeba58388252a579ea13b89
NamDo8467 <huynhnamdo@gmail.com>
2021-06-22 17:50:10 +0700

    finished

--- server/routes/getStudent.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {getStudent} = require('../models/Students')
-
-
-router.get('/:teacher', getStudent)
-module.exports.getStudent = router
